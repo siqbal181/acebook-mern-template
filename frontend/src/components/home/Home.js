447c30fa10fd12c5e7ac5273ff6d6f2605dccbc4
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-10 11:28:07 +0100

    Removed router from Home file and checked its working on page

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
-import LoginForm from '../../auth/LoginForm';
-import SignUpForm from '../../user/SignUpForm';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Acebook</h1>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/signup" component={SignUpForm} />
-        <Route path="/login" component={LoginForm} />
-      </Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
