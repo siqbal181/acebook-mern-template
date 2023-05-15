2ac81f287e1a80221c2bb47119cca984ba86959c
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-05-15 15:45:07 +0100

    frountend started image upload script

--- frontend/src/components/createPost/CreatePostForm.js
+++ frontend/src/components/createPost/CreatePostForm.js
@@ -4,9 +4,18 @@ import {AuthenticationContext} from '../authenticationProvider/AuthenticationPro
 const CreatePostForm = ({ onCreated }) => {
   const {token, username} = useContext(AuthenticationContext)
   const [message, setMessage] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    
+    const formData = new FormData();
+    formData.append("image", imageUrl);
+
+    const image = await fetch('/uploadImage', {
+      method: 'post',
+      body: formData
+    })
 
     let response = await fetch( '/posts', {
       method: 'post',
@@ -19,10 +28,16 @@ const CreatePostForm = ({ onCreated }) => {
 
     setMessage("")
     onCreated();
+    setImageUrl(null);
   }
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value)
+  }
+
+  const handleImageChange = (event) => {
+    console.log(event.target.file[0])
+    setImageUrl(event.target.file[0])
   }
 
     return (
@@ -30,6 +45,7 @@ const CreatePostForm = ({ onCreated }) => {
       <form onSubmit={handleSubmit} id="postForm">
         <h1>Make a post</h1>
         <textarea rows="4" cols="50" placeholder='Write your post here...' id="message" value={ message } onChange={handleMessageChange} form="postForm"/>
+        <input type="file" name="image" onChange={handleImageChange} />
         <input role='submit-button' id='submit' type="submit" value="Submit" />
       </form>
       </>
