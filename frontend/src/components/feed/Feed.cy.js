b97a124bfca3eb20fbceef18dfdc1ba6393b41ba
Sam White <sam.white745@gmail.com>
2023-05-16 16:33:42 +0100

    Updated tests with appropriate return objects

--- frontend/src/components/feed/Feed.cy.js
+++ frontend/src/components/feed/Feed.cy.js
@@ -3,13 +3,26 @@ import { AuthenticationContext } from '../authenticationProvider/AuthenticationP
 const navigate = () => {}
 
 describe("Feed", () => {
+  const fakePost1 = {comments: [],
+    _id: "1",
+    message: "Hello, world",
+    author: "Sam",
+    dateCreated: "2023-05-16T15:00:49.799Z",
+  }
+  const fakePost2 = {
+    comments: [],
+    _id: "2",
+    message: "Hello again, world",
+    author: "Bob",
+    dateCreated: "2023-05-16T15:00:49.799Z",
+  }
   it("Calls the /posts endpoint and lists all the posts", () => {
     cy.intercept('GET', '/posts', (req) => {
       req.reply({
         statusCode: 200,
         body: { posts: [
-          {_id: 1, message: "Hello, world"},
-          {_id: 2, message: "Hello again, world"}
+          fakePost1,
+          fakePost2
         ] }
       })
     }
@@ -30,3 +43,6 @@ describe("Feed", () => {
     })
   })
 })
+
+
+      
