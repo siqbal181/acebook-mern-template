6875009271d2c9c4cf3bdc8c598d562dfbe1e687
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-05-16 16:37:27 +0100

    Profile unfinished but mostly there. Errors with post showing

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,8 +1,9 @@
 import { AuthenticationContext } from '../authenticationProvider/AuthenticationProvider';
 import { useState, useContext, useEffect } from "react";
 
-const UserProfile = ({ }) => {
+const UserProfile = () => {
   const [username, setUsername] = useState("");
+  const [postData, setPostData] = useState([]); 
   const { token, userId } = useContext(AuthenticationContext);
 
   const findUserProfile = async () => {
@@ -14,17 +15,44 @@ const UserProfile = ({ }) => {
       },
     });
     const data = await response.json();
-    console.log(data);
     setUsername(data.username);
+  };
+
+  const getPostsByUser = async () => {
+    try {
+      let response = await fetch(`/posts/${username}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const postData = await response.json();
+      setPostData(postData); // Update the state with the retrieved posts
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
     findUserProfile();
-  }, [userId, token]);
+    getPostsByUser();
+  }, [userId, token, username]); 
 
   return (
     <div className="profile" data-cy='userProfile'>
       <p>Hello {username}!</p>
+      {console.log(postData)};
+      <div id='userposts' role="userposts">
+        <p>Here are your posts:</p>
+        {postData.length > 0 ? (
+          postData.map((post) => (
+            <p key={post._id}>{post.message}</p>
+          ))
+        ) : (
+          <p>No posts found.</p>
+        )}
+      </div>
     </div>
   );
 };
