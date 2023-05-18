54a3e3b0694cb58a7841d4279a1ea7c12ff2c67d
toppy007 <33899845+toppy007@users.noreply.github.com>
2023-05-18 19:48:50 +0100

    comments controller function removed because not used

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const CommentsController = require("../controllers/comments");
 
 router.post("/", CommentsController.createComment);
-router.get("/", CommentsController.getCommentsByPostId);
 
 module.exports = router;
