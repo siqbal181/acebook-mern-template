fe7246af9a5c6298a7d40307cee47d0e42f19ec0
Madelane <madelanedaz@gmail.com>
2023-05-18 17:32:46 +0100

    can upload profile photo from on sign up

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  username: { type: String, required: true }, 
+  username: { type: String, required: true },
+  profilePic: { type: String, default: '/images/default-profile-img.jpeg'} 
 });
 
 const User = mongoose.model("User", UserSchema);
