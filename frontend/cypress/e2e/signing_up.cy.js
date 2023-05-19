13a6ce639eee42c0acd9a3957ac886220d3ce87a
Madelane <madelanedaz@gmail.com>
2023-05-19 12:37:39 +0100

    update on e2e test for signing_up

--- frontend/cypress/e2e/signing_up.cy.js
+++ frontend/cypress/e2e/signing_up.cy.js
@@ -1,12 +1,43 @@
+import 'cypress-file-upload';
+
 describe("Signing up", () => {
-  it("with valid credentials, redirects to '/login'", () => {
+  it("with valid credentials and uploaded photo, redirects to '/login'", () => {
     cy.visit("/signup");
     cy.get("#email").type("someone@example.com");
     cy.get("#password").type("password");
-    cy.get("#username").type("username"); 
+    cy.get("#username").type("username");
+  
+    // Create a fixture file
+    cy.fixture("sun.png").then((fileContent) => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: "sun.png",
+        mimeType: "image/png",
+      });
+    });
+  
+    cy.intercept("POST", "/uploadPhotos", (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          message: "File uploaded successfully",
+          file: {
+            lastModified: "1684192820385",
+            lastModifiedDate: "Tue May 16 2023 00:20:20 GMT+0100 (British Summer Time) {}",
+            name: "sun.png",
+            size: "61074",
+            type: "image/png",
+            webkitRelativePath: "",
+          },
+        },
+      });
+    }).as("fileUpload");
+  
     cy.get("#submit").click();
-
-    cy.url().should("include", "/login");
+  
+    cy.wait("@fileUpload").then(() => {
+      cy.url().should("include", "/login");
+    });
   });
 
   it("with missing password, redirects to '/signup'", () => {
